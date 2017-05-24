10cc627da180a6c25510422363809b25fad0117a
fiatjaf <fiatjaf@gmail.com>
2017-05-24 15:01:01 +0000

    dynamic rule parser with regex.

--- helpers/parser-parser-test.js
+++ helpers/parser-parser-test.js
@@ -8,37 +8,13 @@ tape('parsing user-defined rule definitions', t => {
   t.plan(1)
   t.deepEqual(parseRule(' <word> banana[^@boat] (de|a)'), {
     value: [
-      {
-        kind: 'whitespace'
-      },
-      {
-        kind: 'parameter',
-        name: 'word',
-        type: 'word'
-      },
-      {
-        kind: 'whitespace'
-      },
-      {
-        kind: 'literal',
-        string: 'banana'
-      },
-      {
-        kind: 'optional',
-        alternatives: [
-          '^@boat'
-        ]
-      },
-      {
-        kind: 'whitespace'
-      },
-      {
-        kind: 'alternatives',
-        alternatives: [
-          'de',
-          'a'
-        ]
-      }
+      {kind: 'whitespace'},
+      {kind: 'parameter', name: 'word', type: 'word'},
+      {kind: 'whitespace'},
+      {kind: 'literal', string: 'banana'},
+      {kind: 'optional', alternatives: ['^@boat']},
+      {kind: 'whitespace'},
+      {kind: 'alternatives', alternatives: ['de', 'a']}
     ],
     lexErrors: [],
     parseErrors: []
@@ -46,30 +22,31 @@ tape('parsing user-defined rule definitions', t => {
 })
 
 tape('parsing a line', t => {
-  t.plan(1)
+  t.plan(4)
 
-  let parseLine = makeLineParser([
-    {
-      kind: 'parameter',
-      name: 'xu',
-      type: 'word'
-    },
-    {
-      kind: 'whitespace'
-    },
-    {
-      kind: 'literal',
-      string: 'banana'
-    },
-    {
-      kind: 'optional',
-      alternatives: [
-        '-boat'
-      ]
-    }
+  var parseLine = makeLineParser([
+    {kind: 'parameter', name: 'xu', type: 'word'},
+    {kind: 'whitespace'},
+    {kind: 'literal', string: 'banana'},
+    {kind: 'optional', alternatives: ['-boat']}
   ])
 
-  t.deepEqual(parseLine('goiaba banana-boat'), {
-    xu: 'goiaba'
-  })
+  t.deepEqual(parseLine('açaí banana-boat'), {xu: 'açaí'})
+  t.deepEqual(parseLine('açaí   banana'), {xu: 'açaí'})
+  t.notOk(parseLine('morangos com açúcar'))
+
+  parseLine = makeLineParser([
+    {kind: 'literal', string: 'pag'},
+    {kind: 'optional', alternatives: ['agamento', 'o', 'ou']},
+    {kind: 'whitespace'},
+    {kind: 'parameter', name: 'habitante', type: 'words'},
+    {kind: 'whitespace'},
+    {kind: 'parameter', name: 'valor', type: 'money'},
+    {kind: 'whitespace'},
+    {kind: 'literal', string: 'em'},
+    {kind: 'whitespace'},
+    {kind: 'parameter', type: 'date', name: 'date'}
+  ])
+
+  t.deepEqual(parseLine('pag maria euzébia 525,30 em 13/12/2018'), {habitante: 'maria euzébia', valor: '525,30', date: '13/12/2018'})
 })
